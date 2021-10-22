d65b836035cf83ec894f8bc3fa21fa90a4be6ea4
Rayed <rayed.benbrahim@gmail.com>
2021-10-22 11:45:01 +0200

    refactor: cleaning up code

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,5 +1,6 @@
 import logger from '../utils/logger';
 import getDatabase from '../utils/database'
+import { Database } from 'sqlite';
 
 interface Event {
   eventId: string;
@@ -22,26 +23,25 @@ const handle = async (event: Event) => {
   const db = await getDatabase()
 
   try {
+    await getOrCreateTables(db)
     logger.info('Event received', { event });
-    const {bankAccountId,userId,type} = event.payload
+    const {bankAccountId,userId,type,status} = event.payload
 
     // TODO insert your code here
     console.log(creditSum(event))
-    await db.exec(`CREATE TABLE IF NOT EXISTS balances (
-      bankAccountId TEXT PRIMARY KEY,
-      userId TEXT NOT NULL,
-      balance INTEGER NOT NULL)`)
-    const currentBalance = await db.get('SELECT balance FROM balances WHERE bankAccountId = ?', bankAccountId)
+    const table = getConcernedTable(status)
+    
+    const currentBalance = await db.get(`SELECT balance FROM ${table} WHERE bankAccountId = ?`, bankAccountId)
     let result
     if (!currentBalance) {
       logger.info('First Balance for account ' +  bankAccountId);
     result = await db.run(
-      'INSERT INTO balances (bankAccountId, userId, balance) VALUES (?,?,?)',bankAccountId,userId,event.payload.value
+      `INSERT INTO ${table} (bankAccountId, userId, balance) VALUES (?,?,?)`,bankAccountId,userId,event.payload.value
     )
   } else {
     logger.info('Updating Balance for account ' +  bankAccountId);
     console.log(currentBalance)
-    result = await db.run('UPDATE balances SET balance = ? WHERE bankAccountId = ?',currentBalance, bankAccountId)
+    result = await db.run(`UPDATE ${table} SET balance = ? WHERE bankAccountId = ?`,currentBalance.balance + creditSum(event), bankAccountId)
   }
   console.log(result)
 
@@ -53,9 +53,31 @@ const handle = async (event: Event) => {
   }
 };
 
-const creditSum = (event: Event) => {
+const getConcernedTable = (status: String) => {
+  if (status === 'VALIDATED') return 'balances'
+  return 'nextBalances'
+}
+
+const creditSum = (event: Event):number => {
   if (event.payload.type === 'PAYOUT') return event.payload.value * -1
   return event.payload.value
+}
+
+const getOrCreateTables = async (db: Database) => {
+  try {
+    await db.exec(`CREATE TABLE IF NOT EXISTS balances (
+      bankAccountId TEXT PRIMARY KEY,
+      userId TEXT NOT NULL,
+      balance INTEGER NOT NULL)`)
+    await db.exec(`CREATE TABLE IF NOT EXISTS nextBalances (
+      bankAccountId TEXT PRIMARY KEY,
+      userId TEXT NOT NULL,
+      balance INTEGER NOT NULL)`)
+      logger.info(`Database And Tables Ready`)
+  } catch (err) {
+    logger.error(`error creating tables: ${err}`)
+  }
+  
 }
 
 export default handle;
