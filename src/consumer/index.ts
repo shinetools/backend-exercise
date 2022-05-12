a9dbde7dcafd0f53d19fa0f915f09a153ff62cf9
Cecil A <cecil.arthur@gymlib.com>
2022-05-12 14:10:17 +0200

    TODO retrieved pushed data

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,5 +1,6 @@
 import logger from '../utils/logger';
 import database from '../utils/database'
+
 interface Event {
   eventId: string;
   payload: any;
@@ -28,12 +29,12 @@ enum TransactionType {
 const handle = async (event: Event) => {
   const db = (await database()).db;
   try {
-    logger.info('Event received', { event });
+    //logger.info('Event received', { event });
 
-    const {payload, retry} = event;
-    const transactionAmount = payload.value / 100;
+    const {payload} = event;
+    const transactionAmount = payload?.value / 100;
     // TODO insert your code here
-        // receive event,
+    // receive event,
     // transact from bank account,
     // update account/user Table and Transaction table
     //
@@ -42,60 +43,44 @@ const handle = async (event: Event) => {
     db.serialize(() => {
       const insertTranactionsQuery = db.prepare('INSERT INTO TRANSACTIONS (eventID  , category , userID , bankAccountID , currency , description , transactionCreatedAt , executedAt ,paymentMethod ,status , title , transactionAt , transactionId , type , updatedAt ,value, raw) VALUES(?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
       
-      insertTranactionsQuery.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency,  payload?.description,  payload?.createdAt, payload?.executedAt, payload?.paymentMethod, payload?.status,payload?.title,payload?.transactionAt,payload?.transactionId,payload?.type,payload?.updatedAt,transactionAmount, rawEvent ]);
+      insertTranactionsQuery.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency,  payload?.description,  payload?.createdAt, payload?.executedAt, payload?.paymentMethod, payload?.status,payload?.title,payload?.transactionAt,payload?.transactionId,payload?.type,payload?.updatedAt,transactionAmount, rawEvent ], (error: Error) => {
+        if(error){
+          logger.error(`transaction insert error. eventId: ${event.eventId}`)
+        }
+      });
+      
       insertTranactionsQuery.finalize();
     });
+
 
     if( payload.status !== TransactionStatus.Pending) {
       db.serialize(() => {
         const insertCompletedTransactionQuery = db.prepare('INSERT INTO COMPLETED_TRANSACTION (eventID  , category , userID , bankAccountID , currency ,paymentMethod ,status , transactionId , type ,value) VALUES(?, ?,?,?,?,?,?,?,?,?)');
-        insertCompletedTransactionQuery.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency, payload?.paymentMethod, payload?.status,payload?.transactionId,payload?.type,transactionAmount ]);
+        insertCompletedTransactionQuery.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency, payload?.paymentMethod, payload?.status,payload?.transactionId,payload?.type,transactionAmount ], (error: Error) => {
+          if(error){
+            logger.error(` completed transaction insert error. eventId: ${event.eventId}`)
+          }
+        });
+
         insertCompletedTransactionQuery.finalize();
     
       });
-
-      db.serialize(() => {
-        const insertUserQuery = db.prepare('INSERT INTO USER (ID  , balance , bankAccountID , currency ) VALUES(?, ?,?,?)');
-        const updateUserBalanceQuery = db.prepare('UPDATE USER SET balance = ? WHERE ID = ?')
-        const getUserBalanceQuery = db.prepare('SELECT ID,balance FROM USER WHERE ID = ?');
-        // get existning balance using ID 
-        // based on transaction do the right math.
-
-        //payload?.value
-        let balance = 0;
-        const balanceQueryResult = getUserBalanceQuery.run([payload.userId])
-        console.log('balance: ', balanceQueryResult)
-
-        if(!balanceQueryResult){
-          balance = creditOrDebit(balance, payload.type, transactionAmount)
-          insertUserQuery.run([payload?.userId,balance, payload?.bankAccountId, payload?.currency ]);
-          insertUserQuery.finalize();
-        } else {
-          //TODO: get balance from - getUserBalanceQuery
-          balance = creditOrDebit(balance, payload.type, transactionAmount);
-
-          updateUserBalanceQuery.run([balance,payload?.userId]);
-          updateUserBalanceQuery.finalize();
-        }
-        
-    
-      });
     }
-
-  
-    db.close();
-
-    //logger.info('temp: ', temp)
-
-
-
+/*
+    const res = db.get('SELECT * FROM TRANSACTIONS', (res:any, error: any, row:any) => {
+      console.log('res: ',res);
+      console.log('row: ',row);
+    }); */
+    
     return true;
   } catch (err) {
+    console.log('handler err: ', err)
     return false;
   }
+  
 };
 
 const creditOrDebit = (balance: number, transactionType: TransactionType, amount:number) => {
-  return transactionType === TransactionType.PayIn? balance += amount :  balance-= amount;
+  return transactionType === TransactionType.PayIn? balance += amount :  balance -= amount;
 }
 export default handle;
