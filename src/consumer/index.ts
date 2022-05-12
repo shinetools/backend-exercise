bed47ff769ed84e1a02f73312ccae64d421b324f
Cecil A <cecil.arthur@gymlib.com>
2022-05-12 12:13:28 +0200

    so far...

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,9 +1,15 @@
 import logger from '../utils/logger';
-
+import database from '../utils/database'
 interface Event {
   eventId: string;
   payload: any;
   retry: number;
+}
+
+enum TransactionStatus {
+  Pending = 'PENDING',
+  Canceled = 'CANCELED',
+  Validated ='VALIDATED'
 }
 
 /**
@@ -16,10 +22,51 @@ interface Event {
  * @returns {boolean} false if the event needs to be retried, else true
  */
 const handle = async (event: Event) => {
+  const db = (await database()).db;
   try {
     logger.info('Event received', { event });
 
+    const {payload, retry} = event;
+
     // TODO insert your code here
+        // receive event,
+    // transact from bank account,
+    // update account/user Table and Transaction table
+    //
+  
+    //const temp = db.each('SELECT ID FROM USER')
+    const rawEvent = JSON.stringify(event);
+    db.serialize(() => {
+      const temp = db.prepare('INSERT INTO TRANSACTIONS (eventID  , category , userID , bankAccountID , currency , description , transactionCreatedAt , executedAt ,paymentMethod ,status , title , transactionAt , transactionId , type , updatedAt ,value, raw) VALUES(?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+      
+      temp.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency,  payload?.description,  payload?.createdAt, payload?.executedAt, payload?.paymentMethod, payload?.status,payload?.title,payload?.transactionAt,payload?.transactionId,payload?.type,payload?.updatedAt,payload?.value, rawEvent ]);
+      temp.finalize();
+    });
+
+    if( payload.status !== TransactionStatus.Pending) {
+      db.serialize(() => {
+        const temp = db.prepare('INSERT INTO COMPLETED_TRANSACTION (eventID  , category , userID , bankAccountID , currency ,paymentMethod ,status , title , transactionId , type ,value) VALUES(?, ?,?,?,?,?,?,?,?,?,?)');
+        
+        temp.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency, payload?.paymentMethod, payload?.status,payload?.title,payload?.transactionId,payload?.type,payload?.value ]);
+        temp.finalize();
+    
+      });
+
+      db.serialize(() => {
+        const temp = db.prepare('INSERT INTO COMPLETED_TRANSACTION (eventID  , category , userID , bankAccountID , currency ,paymentMethod ,status , title , transactionId , type ,value) VALUES(?, ?,?,?,?,?,?,?,?,?,?)');
+        
+        temp.run([event.eventId, payload?.category, payload?.userId,  payload?.bankAccountId,  payload?.currency, payload?.paymentMethod, payload?.status,payload?.title,payload?.transactionId,payload?.type,payload?.value ]);
+        temp.finalize();
+    
+      });
+    }
+
+  
+    db.close();
+
+    //logger.info('temp: ', temp)
+
+
 
     return true;
   } catch (err) {
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -11,7 +11,75 @@ const getDatabase = async () => {
     filename: 'test.db',
     driver: sqlite3.Database,
   });
+
+  await userTable(db);
+  await transactionTable(db);
+  await completedTransactionTable(db);
+
   return db;
 };
+
+const userTable = async(db: Database) => {
+  const createUserTableQuery = 'CREATE TABLE USER ( ID VARCHAR(100) , balance NUMBER, bankAccountID VARCHAR(100), currency VARCHAR(10) );';
+  await db.run('SELECT ID from USER WHERE ID = 1', async (err: any) => {
+    if (err) {
+      console.log('Some Error Occured, ', err);
+      await db.run(createUserTableQuery, (err: any) => {
+        if (err) {
+          console.log('Some Error Occured');
+        } else {
+          console.log(' User Table Created');
+        }
+      });
+    } else {
+      console.log('table exists')
+      return;
+    }
+  })
+  
+
+
+}
+const transactionTable = async(db:Database ) => {
+  const createTransactionTableQuery = 'CREATE TABLE TRANSACTIONS ( eventID VARCHAR(100) , category VARCHAR(100), userID VARCHAR(100), bankAccountID VARCHAR(100), currency VARCHAR(10), description VARCHAR(100), transactionCreatedAt VARCHAR(50), executedAt VARCHAR(50),paymentMethod VARCHAR(50),status VARCHAR(50), title VARCHAR(50), transactionAt VARCHAR(50), transactionId VARCHAR(50), type VARCHAR(50), updatedAt VARCHAR(50),value NUMBER, raw VARCHAR(1000)  );';
+
+  await db.run('SELECT eventID from TRANSACTIONS WHERE eventID = 1', async (err: any) => {
+    if (err) {
+      console.log('Some Error Occured, ', err);
+      await db.run(createTransactionTableQuery, (err: any) => {
+        if (err) {
+          console.log('Some Error Occured');
+        } else {
+          console.log(' TRANSACTIONS Table Created');
+        }
+      });
+    } else {
+      console.log('table exists')
+      return;
+    }
+  })
+
+}
+
+const completedTransactionTable = async(db:Database ) => {
+  const createCompletedTransactionTableQuery = 'CREATE TABLE COMPLETED_TRANSACTION ( eventID VARCHAR(100) , category VARCHAR(100), userID VARCHAR(100), bankAccountID VARCHAR(100), currency VARCHAR(10),paymentMethod VARCHAR(50),status VARCHAR(50), transactionId VARCHAR(50), type VARCHAR(50), value NUMBER  );';
+  await db.run('SELECT eventID from COMPLETED_TRANSACTION WHERE eventID = 1', async (err: any) => {
+    if (err) {
+      console.log('Some Error Occured, ', err);
+      await db.run(createCompletedTransactionTableQuery, (err: any) => {
+        if (err) {
+          console.log('Some Error Occured');
+        } else {
+          console.log(' COmpleted COMPLETED_TRANSACTION Table Created');
+        }
+      });
+    } else {
+      console.log('table exists')
+      return;
+    }
+  })
+
+
+}
 
 export default getDatabase;
